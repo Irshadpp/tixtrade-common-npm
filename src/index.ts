d2f1b9d3c1b9bf416b2745b074495611c4595780
irshad <irshad89434@gmail.com>
2024-07-31 13:03:21 +0530

    updates

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,11 @@
-interface Color{
-    a: string;
-    b: string;
-    c: string;
-    d: number
-}
+export * from "./errors/bad-request-error"
+export * from "./errors/custom-error"
+// export * from "./errors/database-connection-error"
+export * from "./errors/not-authorized-error"
+export * from "./errors/not-found-error"
+export * from "./errors/request-validation-error"
 
-const color: Color = {
-    a: "fjdsk",
-    b: "jfdslf",
-    c: "dfjsl",
-    d: 8
-}
-
-export default color;
+export * from "./middlewares/current-user"
+export * from "./middlewares/error-handler"
+export * from "./middlewares/require-auth"
+export * from "./middlewares/validate-request"
