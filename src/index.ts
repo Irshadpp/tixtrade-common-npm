21791a5444375cf6dc0788c8f18f96b88e1b4a61
irshad <irshad89434@gmail.com>
2024-07-31 12:42:09 +0530

    adding configurations

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+interface Color{
+    a: string;
+    b: string;
+    c: string;
+    d: number
+}
+
+const color: Color = {
+    a: "fjdsk",
+    b: "jfdslf",
+    c: "dfjsl",
+    d: 8
+}
+
+export default color;
